90d9ecd92dc956d58e88558c46f74eccede39f91
Dhutchison00 <59940070+Dhutchison00@users.noreply.github.com>
2025-02-21 23:20:16 -0500

    sound effects

--- Assets/Scripts/Appliance.cs
+++ Assets/Scripts/Appliance.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 public class Appliance : Storage
 {
     [SerializeField] private Breakable breakable;
 
     [Tooltip("The QuickTimeEvent to use when interacting with this appliance.")]
     [SerializeReference] private QuickTimeEvent useApplianceQTE = new SliderQTE();
-
-    private bool working  = true;
+    private bool working = true;
 
     public bool InUse() => useApplianceQTE.InProgress();
 
     void Update()
     {
-        if(breakable.CanBreak())
+        if (breakable.CanBreak())
         {
             breakable.HandleBreaking(this);
             breakable.HandleRepairing(zPressed, this);
             working = breakable.IsBroken() == false;
-        }   
+        }
 
-        if(working)
+        if (working)
         {
-            if(useApplianceQTE.QTE(zPressed, xPressed, cPressed, moveInput, this) > 0f)
+            if (useApplianceQTE.QTE(zPressed, xPressed, cPressed, moveInput, this) > 0f)
             {
                 Debug.Log("Success!");
             }
